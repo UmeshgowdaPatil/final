287e26ef81f9416c57a96614d144eef50032d943
GOWDRU <GOWDRU@GOWDRU-PC>
2017-03-14 16:59:06 +0530

    Yappa-2

--- origin/ZZZ/src/newOne/Jump.java
+++ origin/ZZZ/src/newOne/Jump.java
@@ -5,7 +5,6 @@ public class Jump {
 	public static void main(String[] args) {
 		 
 		System.out.println("LOOOOOOOOOOOOOOOOOOooooooooooLLLLLLLLLLLLLL");
-		System.out.println("LOOOOOOOOOOOOOOOOOOooooooooooLLLLLLLLLLLLLL");
 
 	}
 
